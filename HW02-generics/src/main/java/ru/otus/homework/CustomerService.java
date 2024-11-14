7b9c9d0dd601d550805ea8d115877355e2c54860
Илья Саенко <insaenko@spasibosb.ru>
2024-11-14 14:26:16 +0300

    fix copy past, interface customerData

--- HW02-generics/src/main/java/ru/otus/homework/CustomerService.java
+++ HW02-generics/src/main/java/ru/otus/homework/CustomerService.java
@@ -2,25 +2,22 @@ package ru.otus.homework;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class CustomerService {
 
     private final Comparator<Customer> CUSTOMER_SCORES_COMPARATOR = Comparator.comparing(Customer::getScores);
 
-    private final TreeMap<Customer, String> customerData = new TreeMap<>(CUSTOMER_SCORES_COMPARATOR);
+    private final NavigableMap<Customer, String> customerData = new TreeMap<>(CUSTOMER_SCORES_COMPARATOR);
 
     public Map.Entry<Customer, String> getSmallest() {
         Map.Entry<Customer, String> firstEntry = customerData.firstEntry();
         if (firstEntry == null) {
             return null;
         }
-        Customer firstCustomer = firstEntry.getKey();
 
-        return Map.entry(
-                new Customer(firstCustomer.getId(), firstCustomer.getName(), firstCustomer.getScores()),
-                firstEntry.getValue()
-        );
+        return copyEntry(firstEntry);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
@@ -28,15 +25,20 @@ public class CustomerService {
         if (higherEntry == null) {
             return null;
         }
-        Customer higherCustomer = higherEntry.getKey();
 
-        return Map.entry(
-                new Customer(higherCustomer.getId(), higherCustomer.getName(), higherCustomer.getScores()),
-                higherEntry.getValue()
-        );
+        return copyEntry(higherEntry);
     }
 
     public void add(Customer customer, String data) {
         customerData.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> copyEntry(Map.Entry<Customer, String> firstEntry) {
+        Customer firstCustomer = firstEntry.getKey();
+
+        return Map.entry(
+                new Customer(firstCustomer.getId(), firstCustomer.getName(), firstCustomer.getScores()),
+                firstEntry.getValue()
+        );
     }
 }
