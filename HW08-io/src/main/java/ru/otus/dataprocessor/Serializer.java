5d22140f76a7489f7d229ebc7a2ec9ea0dc9eb93
Илья Саенко <insaenko@spasibosb.ru>
2024-12-26 19:50:38 +0300

    throws fixes

--- HW08-io/src/main/java/ru/otus/dataprocessor/Serializer.java
+++ HW08-io/src/main/java/ru/otus/dataprocessor/Serializer.java
@@ -1,10 +1,9 @@
 package ru.otus.dataprocessor;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Map;
 
 public interface Serializer {
 
-    void serialize(Map<String, Double> data) throws IOException, URISyntaxException;
+    void serialize(Map<String, Double> data) throws IOException;
 }
