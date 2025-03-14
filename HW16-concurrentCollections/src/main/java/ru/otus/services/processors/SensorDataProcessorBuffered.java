bd9ff2552b146afbe62ae6d0833a39ae1f04ebd7
Илья Саенко <insaenko@spasibosb.ru>
2025-03-14 15:09:20 +0300

    impl processor buffered

--- HW16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ HW16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,31 +6,44 @@ import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 import ru.otus.lib.SensorDataBufferedWriter;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
+
 // Этот класс нужно реализовать
 @SuppressWarnings({"java:S1068", "java:S125"})
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
+
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
+    private final PriorityBlockingQueue<SensorData> dataBuffer;
+
     private final int bufferSize;
+
     private final SensorDataBufferedWriter writer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-        /*
-            if (dataBuffer.size() >= bufferSize) {
-                flush();
-            }
-        */
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
+        dataBuffer.offer(data);
     }
 
     public void flush() {
         try {
-            // writer.writeBufferedData(bufferedData);
+            List<SensorData> bufferedData = new ArrayList<>(bufferSize);
+            dataBuffer.drainTo(bufferedData);
+            if (!bufferedData.isEmpty()) {
+                writer.writeBufferedData(bufferedData);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
